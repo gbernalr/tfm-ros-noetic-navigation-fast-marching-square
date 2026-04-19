9056b4f2a7a790ea46da0f5f8a7169bd6651a3c4
Guillermo Bernal Ruiz <78974537+gbernalr@users.noreply.github.com>
2026-04-19 18:02:17 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gbernalr/tfm/intro_ws/src
-# Build directory: /home/gbernalr/tfm/intro_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("turtlebot3_fm2_nav")
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/gbernalr/tfm/intro_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/gbernalr/tfm/intro_ws/devel;/opt/ros/noetic'.split(';') if '/home/gbernalr/tfm/intro_ws/devel;/opt/ros/noetic' != '' else []
